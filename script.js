d9ef2aba094958d0f7dbd90a35a46ed826361209
Maebh Ni <mnighuairim@gmail.com>
2024-11-28 13:27:14 +0000

    reconnecting style sheet

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const cards = document.querySelectorAll('.hazard-card');
 
 function flipCard() {
+    // toggle - removes it if it's there, adds it if it's not
     this.classList.toggle('flip');
+
 }
 // This adds an event listener to each card, and calls the flipCard function when clicked
 cards.forEach(card => card.addEventListener('click', flipCard));
