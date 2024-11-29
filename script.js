c65f2c933bd842cacd02b57e1baf10c42a74f1f7
Joanna O'Connor <jtoconnor75@gmail.com>
2024-11-29 14:01:49 +0000

    header bar with buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.getElementById('difficulty-button').addEventListener('change', function() {
+  if (this.value === 'Difficulty level') {
+      var myModal = new bootstrap.Modal(document.getElementById('difficultyModal'), {});
+      myModal.show();
+      this.value = ''; // Reset the select value
+  }
+});
+
 const cards = document.querySelectorAll('.hazard-card');
 
 function flipCard() {
