a1ebc9f049204a4ff711f4473613dc7ce402a190
Maebh Ni <mnighuairim@gmail.com>
2024-11-29 10:35:08 +0000

    adds flipCard function to determine whether a card has already been flipped

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const cards = document.querySelectorAll('.hazard-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
+  // toggle - removes it if it's there, adds it if it's not
   this.classList.toggle('flip');
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log({hasFlippedCard, firstCard});
+  }
 }
 
+// This adds an event listener to each card, and calls the flipCard function when clicked
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 
